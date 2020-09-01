a48ab42b1353d9a9ff147fbd1d4556b49a63708d
FlyElephant <sfmkeso@163.com>
2020-09-01 22:21:11 +0800

    9.用两个栈实现队列

---
+++ 009-QueueWithTwoStacks/src/algorithm/Solution.java
@@ -0,0 +1,47 @@
+package algorithm;
+
+import java.util.Queue;
+import java.util.Stack;
+
+class QueueWithTwoStacks {
+
+    Stack<Integer> s1 = new Stack<>();
+    Stack<Integer> s2 = new Stack<>();
+
+    public void offer(Integer val) {
+        s1.push(val);
+    }
+
+    public Integer poll() {
+        if (s1.size() == 0 && s2.size() == 0) {
+            try {
+                throw new Exception("empty queue");
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        } else {
+            if (s2.size() == 0) {
+                while (!s1.isEmpty()) {
+                    s2.push(s1.pop());
+                }
+            }
+            return s2.pop();
+        }
+
+        return s2.peek();
+    }
+}
+
+public class Solution {
+
+    public static void main(String[] args) {
+        QueueWithTwoStacks queue = new QueueWithTwoStacks();
+        queue.offer(1);
+        queue.offer(2);
+        queue.offer(3);
+        System.out.println(queue.poll());
+        System.out.println(queue.poll());
+        System.out.println(queue.poll());
+        System.out.println(queue.poll());
+    }
+}
