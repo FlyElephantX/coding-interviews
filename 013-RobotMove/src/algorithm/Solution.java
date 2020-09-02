b1720a7bfd8cf57e9e0525d85f25c936738e419b
FlyElephant <sfmkeso@163.com>
2020-09-03 00:10:24 +0800

    13.机器人的运动范围

---
+++ 013-RobotMove/src/algorithm/Solution.java
@@ -0,0 +1,52 @@
+package algorithm;
+
+class RobotMove {
+
+    public int movingCount(int threshold, int rows, int cols) {
+        if (threshold < 0 || rows < 0 || cols < 0) {
+            return 0;
+        }
+        boolean[] visited = new boolean[rows * cols];
+        int count = movingCountCore(threshold, rows, cols, 0, 0, visited);
+        return count;
+    }
+
+    private int movingCountCore(int threshold, int rows, int cols, int row, int col, boolean[] visited) {
+        int count = 0;
+        if (check(threshold, rows, cols, row, col, visited)) {
+            visited[row * cols + col] = true;
+            count = 1 + movingCountCore(threshold, rows, cols, row + 1, col, visited)
+                    + movingCountCore(threshold, rows, cols, row - 1, col, visited)
+                    + movingCountCore(threshold, rows, cols, row, col + 1, visited)
+                    + movingCountCore(threshold, rows, cols, row, col - 1, visited);
+        }
+        return count;
+
+    }
+
+    private boolean check(int threshold, int rows, int cols, int row, int col, boolean[] visited) {
+        if (row >= 0 && row < rows && col >= 0 && col < cols && !visited[row * cols + col] && (getDigitSum(row) + getDigitSum(col) <= threshold)) {
+            return true;
+        }
+        return false;
+    }
+
+    private int getDigitSum(int number) {
+        int sum = 0;
+        while (number > 0) {
+            sum += number % 10;
+            number = number / 10;
+        }
+        return sum;
+    }
+
+}
+
+public class Solution {
+
+    public static void main(String[] args) {
+        RobotMove move = new RobotMove();
+        int count = move.movingCount(10,3,3);
+        System.out.println("格子总数:" + count);
+    }
+}
