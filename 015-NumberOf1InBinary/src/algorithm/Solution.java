4b26e07a962ae1faa6e32edd56e090a08a7a8925
FlyElephant <sfmkeso@163.com>
2020-09-03 20:48:31 +0800

    15.二进制中1的个数

---
+++ 015-NumberOf1InBinary/src/algorithm/Solution.java
@@ -0,0 +1,23 @@
+package algorithm;
+
+class NumberOf1InBinary {
+
+    public int numberOf1(int n) {
+        int count = 0;
+        while (n != 0) {
+            count++;
+            n = n & (n - 1);
+        }
+        return count;
+    }
+
+}
+
+public class Solution {
+
+    public static void main(String[] args) {
+        NumberOf1InBinary binary = new NumberOf1InBinary();
+        int count = binary.numberOf1(15);
+        System.out.println("1的个数:" + count);
+    }
+}
