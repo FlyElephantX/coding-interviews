eb9293d5393bd6698d92cbc61e987a98d237a4d5
FlyElephant <sfmkeso@163.com>
2020-09-07 22:32:45 +0800

    44.数字序列中某一位的数字

---
+++ 044-DigitsInSequence/src/algorithm/Solution.java
@@ -0,0 +1,54 @@
+package algorithm;
+
+class DigitsInSequence {
+
+    public int digitAtIndex(int index) {
+        if (index < 0) {
+            return -1;
+        }
+        int digits = 1;
+        while (true) {
+            int numbers = countOfIntegers(digits);
+            if (index < numbers * digits) {
+                return digitAtIndex(index, digits);
+            }
+            index -= numbers * digits;
+            digits++;
+        }
+    }
+
+    public int digitAtIndex(int index, int digits) {
+        int number = beginNumber(digits) + index / digits;
+        // 从左到右第m位，就是从右到左的第digits-m位
+        int indexFromRight = digits - index % digits;
+        for (int i = 1; i < indexFromRight; i++) {
+            number /= 10;
+        }
+        return number % 10;
+    }
+
+    public int countOfIntegers(int digits) {
+        if (digits == 1) {
+            return 10;
+        }
+        int count = (int) Math.pow(10,digits - 1);
+        return 9 * count;
+
+    }
+
+    public int beginNumber(int digits) {
+        if (digits == 1) {
+            return 0;
+        }
+        return  (int) Math.pow(10, digits - 1);
+    }
+}
+
+public class Solution {
+
+    public static void main(String[] args) {
+        DigitsInSequence sequence = new DigitsInSequence();
+        int res = sequence.digitAtIndex(1001);
+        System.out.println("数字序列中某一位的数字:" + res);
+    }
+}
