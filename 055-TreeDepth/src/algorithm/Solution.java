600dc4010bf2c2c18ad3df169774e530529dabeb
FlyElephant <sfmkeso@163.com>
2020-09-09 23:40:58 +0800

    55.二叉树的深度

---
+++ 055-TreeDepth/src/algorithm/Solution.java
@@ -0,0 +1,48 @@
+package algorithm;
+
+class BinaryTreeNode {
+    int val;
+    BinaryTreeNode left;
+    BinaryTreeNode right;
+
+    public BinaryTreeNode(int val) {
+        this.val = val;
+    }
+}
+
+class TreeDepth {
+
+    public int treeDepth(BinaryTreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        if (root.left == null && root.right == null) {
+            return 1;
+        }
+        int left = treeDepth(root.left);
+        int right = treeDepth(root.right);
+        return left > right ? left + 1 : right + 1;
+    }
+}
+
+public class Solution {
+
+    public static void main(String[] args) {
+        BinaryTreeNode node1 = new BinaryTreeNode(1);
+        BinaryTreeNode node2 = new BinaryTreeNode(2);
+        BinaryTreeNode node3 = new BinaryTreeNode(3);
+        BinaryTreeNode node4 = new BinaryTreeNode(4);
+        BinaryTreeNode node5 = new BinaryTreeNode(5);
+        BinaryTreeNode node6 = new BinaryTreeNode(6);
+        BinaryTreeNode node7 = new BinaryTreeNode(7);
+        node1.left = node2;
+        node1.right = node3;
+        node2.left = node4;
+        node2.right = node5;
+        node3.right = node6;
+        node5.left = node7;
+        TreeDepth treeDepth = new TreeDepth();
+        int depth = treeDepth.treeDepth(node1);
+        System.out.println("二叉树的深度:" + depth);
+    }
+}
