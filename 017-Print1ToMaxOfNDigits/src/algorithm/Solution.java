454ac6b25b1b0f1c4f710cb885e018f5440b6cb9
xiangdefei <xiangdefei@xiaomi.com>
2021-02-23 17:55:13 +0800

    字符串表示大数递增

--- 017-Print1ToMaxOfNDigits/src/algorithm/Solution.java
+++ 017-Print1ToMaxOfNDigits/src/algorithm/Solution.java
@@ -25,6 +25,40 @@ class Print1ToMaxOfNDigits {
         }
     }
 
+    public void printNumbers1(int n) {
+        StringBuilder str = new StringBuilder();
+        // 将str初始化为n个'0'字符组成的字符串
+        for (int i = 0; i < n; i++) {
+            str.append('0');
+        }
+        while(!increment(str)){
+            // 去掉左侧的0
+            int index = 0;
+            while (index < str.length() && str.charAt(index) == '0'){
+                index++;
+            }
+            System.out.println(str.toString().substring(index));
+        }
+    }
+
+    public boolean increment(StringBuilder str) {
+        boolean isOverflow = false;
+        for (int i = str.length() - 1; i >= 0; i--) {
+            char s = (char)(str.charAt(i) + 1);
+            // 如果s大于'9'则发生进位
+            if (s > '9') {
+                str.replace(i, i + 1, "0");
+                if (i == 0) {
+                    isOverflow = true;
+                }
+            } else { // 末尾不超过9，进行正常替换
+                str.replace(i, i + 1, String.valueOf(s));
+                break;
+            }
+        }
+        return isOverflow;
+    }
+
 }
 
 public class Solution {
@@ -33,5 +67,6 @@ public class Solution {
         Print1ToMaxOfNDigits digits = new Print1ToMaxOfNDigits();
         String res = digits.printNumbers(2);
         System.out.println(res);
+        digits.printNumbers1(2);
     }
 }
